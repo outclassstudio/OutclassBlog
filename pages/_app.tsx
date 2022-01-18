ef60f2c1a34a5f996cd0e242f400d57b357a2496
outclassstudio <mlimshs@gmail.com>
2022-01-18 20:29:43 +0900

    Feat: Set interface

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { NextPageContext } from "next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,3 +12,16 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+MyApp.getInitialProps = async (context: NextPageContext) => {
+  const { ctx, Component }: any = context;
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    // Component의 context로 ctx를 넣어주자
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  // return한 값은 해당 컴포넌트의 props로 들어가게 됩니다.
+  return { pageProps };
+};
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,60 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import Seo from "../components/Seo";
 
-const Contact: NextPage = () => {
+const Contact: NextPage = ({ props }: any) => {
+  console.log(props);
+
   return (
-    <div>
+    <div className="container">
       <Seo title="Contact" />
+      <div className="main">
+        <div>문의 : outclassstudio@gmail.com</div>
+      </div>
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: center;
+        }
+        .main {
+          width: 1000px;
+          display: flex;
+          /* flex-direction: column; */
+          background-color: white;
+          margin-top: 30px;
+          padding: 20px;
+          border: 1px solid #e9e9e9;
+          justify-content: center;
+        }
+        .main img {
+          width: 450px;
+        }
+        .left {
+          width: 485px;
+        }
+        .right {
+          /* flex: 1 0 auto; */
+          width: 485px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+        }
+        .right div {
+          margin: 10px;
+          font-size: 20px;
+        }
+        .right a {
+          margin: 10px;
+        }
+      `}</style>
     </div>
   );
 };
 
 export default Contact;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch(`http://localhost:3000/api/movies`);
+  const data = await res.json();
+
+  return { props: { data } };
+};
