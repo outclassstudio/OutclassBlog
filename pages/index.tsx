f2aea87bde09d2f7884393472cf96c5a98bbc383
outclass <mhlee@wemb.co.kr>
2022-10-28 17:00:11 +0900

    Feat : Sorting movie list(2)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,12 @@ import type { GetServerSideProps, GetStaticProps, NextPage } from "next";
 import MoviePreview from "../components/MoviePreview";
 import { useRouter } from "next/router";
 import Seo from "../components/Seo";
-import ImageTag from "next/image";
 import styled from "styled-components";
 import axios from "axios";
 import { mediaQuery } from "../styles/global.style";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FlexColumnDiv, FlexColumnDivCentered, FlexDiv } from "../styles/utility.style";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import MovieBox from "../components/MovieBox";
 
 interface IMovieProps {
   movies: MovieTypes.Movie[];
@@ -20,19 +18,26 @@ type SortedMovieType = null | MovieTypes.Movie[]
 
 const Home: NextPage<IMovieProps> = ({ movies, recommended }) => {
   const [sortedMovieList, setSortedMovieList] = useState<SortedMovieType>(null)
-  const router = useRouter();
-  const navigate = (id: number, title: string) => {
-    router.push(`/movies/${title}/${id}`);
-  };
 
-  const handleSortingMovieList = () => {
-    const sorted = movies?.sort((a,b) => Number(b.vote_average) - Number(a.vote_average))
-    if(sorted) {
-      setSortedMovieList(sorted)
-    } 
+  const handleSortingMovieList = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    let sorted: SortedMovieType
+
+    if(e.target.value === "평점순") {
+      sorted = movies?.sort((a,b) => Number(b.vote_average) - Number(a.vote_average))
+      if(sorted.length) {
+        setSortedMovieList(sorted)
+      } 
+    } else if(e.target.value === "최신순") {
+      sorted = movies?.sort((a,b) =>{
+        const date1:any = new Date(a.release_date);
+        const date2:any = new Date(b.release_date);
+        return date2.getTime() - date1.getTime();
+      })
+      if(sorted.length) {
+        setSortedMovieList(sorted)
+      } 
+    }
   }
-
-  // console.log(movies)
 
   return (
     <MainContainer>
@@ -42,61 +47,20 @@ const Home: NextPage<IMovieProps> = ({ movies, recommended }) => {
         <MainHeader>
           <HeaderText>
             TMDB 최신 영화
+            <SelectBox onChange={handleSortingMovieList}>
+              <option>옵션을 선택하세요</option>
+              <option>최신순</option>
+              <option>평점순</option>
+            </SelectBox>
           </HeaderText>
-          <select onChange={handleSortingMovieList}>
-            <option>최신순</option>
-            <option>평점순</option>
-          </select>
         </MainHeader>
         <GridContainer>
           {sortedMovieList === null ?
           (movies.map((movie: MovieTypes.Movie) => (
-            <MovieBox key={movie.id}>
-              <ImageWrapper>
-                <ImageTag
-                  onClick={() => navigate(movie.id, movie.original_title)}
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt="/Logo.png"
-                  layout="fill"
-                />
-              </ImageWrapper>
-              <MovieInfoWrapper>
-                <MovieTitle
-                  onClick={() => navigate(movie.id, movie.original_title)}
-                >
-                  {movie.original_title}
-                </MovieTitle>
-                <MovieInfoSubWrapper>
-                  <MovieInfo>{movie.release_date}</MovieInfo>
-                  <FontAwesomeIcon icon={faStar} color={"#ffaf4c"} width={10} />
-                  <MovieInfo>{movie.vote_average}</MovieInfo>
-                </MovieInfoSubWrapper>
-              </MovieInfoWrapper>
-            </MovieBox>
+            <MovieBox key={movie.id} movie={movie}/>
           )))
-        : (sortedMovieList.map((movie: MovieTypes.Movie) => (
-          <MovieBox key={movie.id}>
-            <ImageWrapper>
-              <ImageTag
-                onClick={() => navigate(movie.id, movie.original_title)}
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                alt="/Logo.png"
-                layout="fill"
-              />
-            </ImageWrapper>
-            <MovieInfoWrapper>
-              <MovieTitle
-                onClick={() => navigate(movie.id, movie.original_title)}
-              >
-                {movie.original_title}
-              </MovieTitle>
-              <MovieInfoSubWrapper>
-                <MovieInfo>{movie.release_date}</MovieInfo>
-                <FontAwesomeIcon icon={faStar} color={"#ffaf4c"} width={10} />
-                <MovieInfo>{movie.vote_average}</MovieInfo>
-              </MovieInfoSubWrapper>
-            </MovieInfoWrapper>
-          </MovieBox>
+        : (sortedMovieList?.map((movie: MovieTypes.Movie) => (
+            <MovieBox key={movie.id} movie={movie}/>
         )))
         }
         </GridContainer>
@@ -149,10 +113,11 @@ const MainHeader = styled(FlexDiv)`
   width: 100%;
   padding: 0px 25px;
   margin-top: 20px;
+  align-items: center;
 `;
 const HeaderText = styled(FlexDiv)`
   width: 100%;
-  justify-content: left;
+  justify-content: space-between;
   font-size: 18px;
   font-weight: 700;
   padding-bottom : 15px;
@@ -178,62 +143,6 @@ const GridContainer = styled.div`
     grid-template-columns: repeat(2, 1fr);
   }
 `;
-const MovieBox = styled(FlexColumnDiv)`
-  align-items: center;
-  position: relative;
-`;
-const ImageWrapper = styled.div`
-  width: 225px;
-  height: 337px;
-  transition: transform 0.2s ease-in-out;
-  box-shadow: rgba(0, 0, 0, 0.103) 0px 4px 12px;
-  position: relative;
-  border-radius: 7px;
-  cursor: pointer;
-
-  img {
-    object-fit: contain;
-    border-radius: 7px;
-  }
-
-  ${mediaQuery.middle} {
-    width: 180px;
-    height: 270px;
-  }
-
-  ${mediaQuery.mobile} {
-    width: 135px;
-    height: 202px;
-  }
-
-  &:hover {
-    transform: scale(1.05) translateY(-10px);
-  }
-`;
-const MovieInfoWrapper = styled(FlexColumnDiv)`
-  width: 100%;
-
-  ${mediaQuery.middle} {
-    padding : 0px 10px;
-  }
-
-  ${mediaQuery.mobile} {
-    padding : 0px 5px;
-  }
-`
-const MovieTitle = styled.div`
-  font-size: 14px;
-  font-weight: 600;
-
-  ${mediaQuery.mobile} {
-    font-size: 12px;
-  }
-`;
-const MovieInfoSubWrapper = styled(FlexDiv)`
-  display: flex;
-  gap: 5px;
-`
-const MovieInfo = styled.div`
-  font-size : 11px;
-  font-weight: 300;
+const SelectBox = styled.select`
+  height: 25px;
 `
